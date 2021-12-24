416d3c32257cd5be637b359fc36517dff29e4fc9
ihesah1 <Hesah.Shaman.elq.js@tuwaiq.edu.sa>
2021-12-24 21:21:40 +0300

    Create new Episode 🥳

--- Routers/controllers/podcast.js
+++ Routers/controllers/podcast.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 const mongoose = require("mongoose");
 const { Podcast, validatePodcast } = require("./../../db/models/podcast");
 const { User } = require("./../../db/models/user");
+const { Episode, validateEpisode } = require("./../../db/models/episode");
 const { dirname } = require("path");
 const { v4: uuidv4 } = require("uuid");
 
@@ -96,6 +97,95 @@ module.exports = {
           .json({ success: false, message: "Podcast does not exist" });
 
       res.send({ success: true, podcast });
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+// Create new Episode 
+  newEpisode: async (req, res) => {
+    try {
+      const { error } = validateEpisode(req.body);
+      if (error)
+        return res
+          .status(400)
+          .send({ success: false, message: error.details[0].message });
+
+      if (!mongoose.Types.ObjectId.isValid(req.body.podcastId))
+        return res
+          .status(400)
+          .json({ success: false, message: "Invalid podcast id" });
+
+      //check if user uploaded audio file
+      if (req.files === null)
+        return res
+          .status(400)
+          .json({ success: false, message: "No audio file is selected" });
+
+      const audioFile = req.files.audio;
+
+      let audio_id = uuidv4();
+      const appDir = dirname(require.main.filename);
+      audioFile.mv(
+        `${appDir}/public/uploads/${audio_id + "_" + audioFile.name}`
+      );
+
+      let audio = `${audio_id + "_" + audioFile.name}`;
+
+      let episode = new Episode({
+        ..._.pick(req.body, ["name", "podcastId", "title", "date"]),
+        audio,
+      });
+
+      await episode.save();
+
+      res.send({
+        success: true,
+        episode: _.pick(episode, [
+          "_id",
+          "name",
+          "podcastId",
+          "title",
+          "date",
+          "audio",
+        ]),
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  searchPodcast: async (req, res) => {
+    const { query } = req.query;
+    try {
+      let results = await Podcast.find().select("-__v");
+
+      if (query === undefined || query === "")
+        return res.send({ success: true, podcasts: results });
+
+      let podcasts = [];
+
+      podcasts = results.filter(
+        (result) =>
+          result.name.toLowerCase().includes(query.toLowerCase()) ||
+          result.description.toLowerCase().includes(query.toLowerCase())
+      );
+
+      res.send({ success: true, podcasts });
+    } catch (error) {
+      console.log(error.message);
+    }
+  },
+  searchPodcastByCategory: async (req, res) => {
+    try {
+      const { category } = req.params;
+
+      let results = await Podcast.find().select("-__v");
+      let podcasts = [];
+
+      podcasts = results.filter(
+        (result) => result.category.toLowerCase() === category.toLowerCase()
+      );
+
+      res.send({ success: true, podcasts });
     } catch (error) {
       console.log(error.message);
     }
--- Routers/routes/podcast.js
+++ Routers/routes/podcast.js
@@ -3,8 +3,11 @@ const { createPodcast, getAllPodcasts, getOnePodcast } = require("../controllers
 
 const auth = require("../middleware/auth");
 
+router.get("/search/all", auth, searchPodcast);
+router.get("/search/category/:category", auth, searchPodcastByCategory);
 router.get("/", auth, getAllPodcasts);
 router.get("/:id", auth, getOnePodcast);
 router.post("/", auth, createPodcast);
+router.post("/episode", auth, newEpisode);
 
 module.exports = router;
